267870c5b49ae02c52c1ab57f56573446832f592
Gustav Hemma <gustav.ekstrom@elev.ga.lbs.se>
2025-01-31 00:07:09 +0100

    Save system now working

--- Project/Scripts/Save System/Data.cs
+++ Project/Scripts/Save System/Data.cs
@@ -9,8 +9,12 @@ namespace MineExploration
 {
     public class Data
     {
-        public float baseMovementSpeed;
-        public float currentHealth;
-        public Vector2 savedPosition;
+        // * ALL VARIABLES NEED TO BE PROPERTIES * \\
+
+        public float BaseMovementSpeed { get; set; } = PlayerStats.BaseMovmentSpeed;
+        public float MaxHealth { get; set; } = PlayerStats.MaxHealth;
+
+        // Index 1 in array = x axes, index 2 in array = y axes
+        public float[] SavedPosition { get; set; } = new float[2];
     }
 }
